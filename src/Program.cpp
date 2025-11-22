e859a29b340711c53398879c305cce0e219f38ed
fwno1y <20070119@sjtu.edu.cn>
2025-11-22 16:08:08 +0800

    Program done

--- src/Program.cpp
+++ src/Program.cpp
@@ -4,4 +4,49 @@
 
 Program::Program() : programCounter_(-1), programEnd_(false) {}
 
+void Program::addStmt(int line, Statement* stmt) {
+  recorder_.add(line,stmt);
+}
+void Program::removeStmt(int line) {
+  recorder_.remove(line);
+}
+
+void Program::run() {
+  resetAfterRun();
+  programCounter_ = recorder_.nextLine(0);
+  while (!programEnd_ && programCounter_ != -1) {
+    const Statement* stmt = recorder_.get(programCounter_);
+    stmt->execute(vars_,*this);
+    programCounter_ = recorder_.nextLine(programCounter_);
+  }
+}
+
+void Program::list() const {
+  recorder_.printLines();
+}
+void Program::clear() {
+  recorder_.clear();
+  vars_.clear();
+}
+
+void Program::execute(Statement* stmt) {
+  stmt->execute(vars_,*this);
+}
+
+int Program::getPC() const noexcept {
+  return programCounter_;
+}
+
+void Program::changePC(int line) {
+  programCounter_ = line;
+}
+
+void Program::programEnd() {
+  programEnd_ = true;
+}
+
+void Program::resetAfterRun() noexcept {
+  programCounter_ = -1;
+  programEnd_ = false;
+}
 // TODO: Imply interfaces declared in the Program.hpp.
