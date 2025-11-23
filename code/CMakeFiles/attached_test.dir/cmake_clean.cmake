f88bf7b7b38a714f1f68445c0be3e0b6ecac199d
fwno1y <20070119@sjtu.edu.cn>
2025-11-23 12:10:25 +0800

    generate code

---
+++ code/CMakeFiles/attached_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/attached_test.dir/AttachedTest.cpp.o"
+  "CMakeFiles/attached_test.dir/AttachedTest.cpp.o.d"
+  "attached_test"
+  "attached_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/attached_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/src/Basic.cpp.o"
+  "CMakeFiles/code.dir/src/Basic.cpp.o.d"
+  "CMakeFiles/code.dir/src/Expression.cpp.o"
+  "CMakeFiles/code.dir/src/Expression.cpp.o.d"
+  "CMakeFiles/code.dir/src/Lexer.cpp.o"
+  "CMakeFiles/code.dir/src/Lexer.cpp.o.d"
+  "CMakeFiles/code.dir/src/Parser.cpp.o"
+  "CMakeFiles/code.dir/src/Parser.cpp.o.d"
+  "CMakeFiles/code.dir/src/Program.cpp.o"
+  "CMakeFiles/code.dir/src/Program.cpp.o.d"
+  "CMakeFiles/code.dir/src/Recorder.cpp.o"
+  "CMakeFiles/code.dir/src/Recorder.cpp.o.d"
+  "CMakeFiles/code.dir/src/Statement.cpp.o"
+  "CMakeFiles/code.dir/src/Statement.cpp.o.d"
+  "CMakeFiles/code.dir/src/Token.cpp.o"
+  "CMakeFiles/code.dir/src/Token.cpp.o.d"
+  "CMakeFiles/code.dir/src/VarState.cpp.o"
+  "CMakeFiles/code.dir/src/VarState.cpp.o.d"
+  "CMakeFiles/code.dir/src/utils/Error.cpp.o"
+  "CMakeFiles/code.dir/src/utils/Error.cpp.o.d"
+  "code"
+  "code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
