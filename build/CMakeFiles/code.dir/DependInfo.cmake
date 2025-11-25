0b5de822f54f3ddd85b00391d4a3e3528466d253
fwno1y <20070119@sjtu.edu.cn>
2025-11-25 19:21:41 +0800

    gitignore changed

---
+++ build/CMakeFiles/code.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/36433/Desktop/课程作业/程序设计/BasicInterpreter-2025-main/src/Basic.cpp" "CMakeFiles/code.dir/src/Basic.cpp.o" "gcc" "CMakeFiles/code.dir/src/Basic.cpp.o.d"
+  "/mnt/c/Users/36433/Desktop/课程作业/程序设计/BasicInterpreter-2025-main/src/Expression.cpp" "CMakeFiles/code.dir/src/Expression.cpp.o" "gcc" "CMakeFiles/code.dir/src/Expression.cpp.o.d"
+  "/mnt/c/Users/36433/Desktop/课程作业/程序设计/BasicInterpreter-2025-main/src/Lexer.cpp" "CMakeFiles/code.dir/src/Lexer.cpp.o" "gcc" "CMakeFiles/code.dir/src/Lexer.cpp.o.d"
+  "/mnt/c/Users/36433/Desktop/课程作业/程序设计/BasicInterpreter-2025-main/src/Parser.cpp" "CMakeFiles/code.dir/src/Parser.cpp.o" "gcc" "CMakeFiles/code.dir/src/Parser.cpp.o.d"
+  "/mnt/c/Users/36433/Desktop/课程作业/程序设计/BasicInterpreter-2025-main/src/Program.cpp" "CMakeFiles/code.dir/src/Program.cpp.o" "gcc" "CMakeFiles/code.dir/src/Program.cpp.o.d"
+  "/mnt/c/Users/36433/Desktop/课程作业/程序设计/BasicInterpreter-2025-main/src/Recorder.cpp" "CMakeFiles/code.dir/src/Recorder.cpp.o" "gcc" "CMakeFiles/code.dir/src/Recorder.cpp.o.d"
+  "/mnt/c/Users/36433/Desktop/课程作业/程序设计/BasicInterpreter-2025-main/src/Statement.cpp" "CMakeFiles/code.dir/src/Statement.cpp.o" "gcc" "CMakeFiles/code.dir/src/Statement.cpp.o.d"
+  "/mnt/c/Users/36433/Desktop/课程作业/程序设计/BasicInterpreter-2025-main/src/Token.cpp" "CMakeFiles/code.dir/src/Token.cpp.o" "gcc" "CMakeFiles/code.dir/src/Token.cpp.o.d"
+  "/mnt/c/Users/36433/Desktop/课程作业/程序设计/BasicInterpreter-2025-main/src/VarState.cpp" "CMakeFiles/code.dir/src/VarState.cpp.o" "gcc" "CMakeFiles/code.dir/src/VarState.cpp.o.d"
+  "/mnt/c/Users/36433/Desktop/课程作业/程序设计/BasicInterpreter-2025-main/src/utils/Error.cpp" "CMakeFiles/code.dir/src/utils/Error.cpp.o" "gcc" "CMakeFiles/code.dir/src/utils/Error.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/scope_test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/36433/Desktop/课程作业/程序设计/BasicInterpreter-2025-main/ScopeTest.cpp" "CMakeFiles/scope_test.dir/ScopeTest.cpp.o" "gcc" "CMakeFiles/scope_test.dir/ScopeTest.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
